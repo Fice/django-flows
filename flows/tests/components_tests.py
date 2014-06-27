e57af20cd6deadc3a0ce135eca932045d19726c2
Joao Pimentel <jpimentel@laterpay.net>
2014-06-27 16:05:30 +0100

    test non_atomic_requests view annotation

--- flows/tests/components_tests.py
+++ flows/tests/components_tests.py
@@ -1,7 +1,8 @@
 
 import unittest
+from django.conf.urls import include, patterns
 from flows.components import Action, Scaffold
-
+from flows.handler import FlowHandler
 
 class Action1(Action):
     url = '/1$'
@@ -12,6 +13,11 @@ class Action2(Action):
 class Scaffold1(Scaffold):
     url = '/scaff1'
     action_set = [Action1, 'Action2']
+
+
+class NonAtomicAction(Action):
+    url = '/nonatomic$'
+    non_atomic_requests = True
 
 
 class LazyActionSetTest(unittest.TestCase):
@@ -25,3 +31,18 @@ class LazyActionSetTest(unittest.TestCase):
         actions = Scaffold1().action_set
         self.assertEqual( 0, actions.index(Action1) )
         self.assertEqual( 1, actions.index(Action2) )
+
+
+class NonAtomicRequestsTest(unittest.TestCase):
+
+    def test_non_atomic_requests(self):
+        handler = FlowHandler('non_atomic')
+        handler.register_entry_point(NonAtomicAction)
+        urlpatterns = patterns('',
+            (r'', include(handler.urls)),
+        )
+        view_function = urlpatterns[0].resolve('/nonatomic').func
+        # If view function was annotated with
+        # django.db.transaction.non_atomic_requests, then the view function 
+        # should have a _non_atomic_requests attribute
+        self.assertTrue(hasattr(view_function, '_non_atomic_requests'))
